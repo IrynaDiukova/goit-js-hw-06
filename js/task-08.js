a9226affd7503416603f5c52e4e2a1cec7ab23dd
Iryna Hridnova <diukovairyna@ukr.net>
2022-10-04 19:05:56 +0300

    add9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector('.login-form');
+
+  const formData = {}
+
+formRef.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event){
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (!mail|| !password) {
+        alert("Все поля дожны быть заполены!");
+      }
+
+    event.currentTarget.reset();
+  
+    console.log({"email": mail, "password": password});
+}
 
